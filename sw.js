9a01e913bd79402b0e8e317a0fdd2e50aed3b7af
grzegorzjankwiatosz-artist <grzegorz.jan.kwiatosz@gmail.com>
2026-04-26 21:37:33 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'anglo-sapiens-v1';
+const ASSETS = [
+  '/anglo-sapiens/',
+  '/anglo-sapiens/index.html',
+  '/anglo-sapiens/manifest.json',
+  '/anglo-sapiens/unit1/index.html',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (res && res.status === 200 && res.type === 'basic') {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => caches.match('/anglo-sapiens/index.html'));
+    })
+  );
+});
